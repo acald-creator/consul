4aa1dbe5aec7c381a343ddd54b2200eeaa202bee
Jared Kirschner <jkirschner@hashicorp.com>
2022-07-15 16:05:53 -0700

    Fix panic on acl token read with -self and -expanded

--- command/acl/token/read/token_read.go
+++ command/acl/token/read/token_read.go
@@ -92,6 +92,11 @@ func (c *cmd) Run(args []string) int {
 			return 1
 		}
 	} else {
+		if c.expanded {
+			c.UI.Error("Cannot use both -expanded and -self. Instead, use -expanded and -id=<accessor id>.")
+			return 1
+		}
+
 		t, _, err = client.ACL().TokenReadSelf(nil)
 		if err != nil {
 			c.UI.Error(fmt.Sprintf("Error reading token: %v", err))
